2e5ba870bc11bb1cd9dcd132736334ea21bb8fc4
sim <git@sgougeon.fr>
2026-03-08 10:23:38 +0100

    Fix abortion on serialization error

--- src/main/java/org/thoughtcrime/securesms/service/UnifiedPushService.java
+++ src/main/java/org/thoughtcrime/securesms/service/UnifiedPushService.java
@@ -48,6 +48,7 @@ public class UnifiedPushService extends PushService {
     String token = serializeForNotifiers(pushEndpoint);
     if (token == null) {
       Log.e(TAG, "Couldn't serialize token, aborting.");
+      return;
     }
     getDcAccounts().setPushDeviceToken(token);
     KeepAliveService.maybeStopSelf(this);
